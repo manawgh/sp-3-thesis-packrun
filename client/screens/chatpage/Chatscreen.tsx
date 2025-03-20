21e3609c019242c7ab5a5cd5064b1ad6a87c43a7
archiemt <archiemt7@gmail.com>
2025-03-20 17:31:52 +0000

    socket + removed dupe client

--- client/screens/chatpage/Chatscreen.tsx
+++ client/screens/chatpage/Chatscreen.tsx
@@ -1,15 +1,33 @@
 import { Text, View, TextInput, FlatList, TouchableOpacity, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import styles from './styles';
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { io } from 'socket.io-client';
+// import {v4 as uuidv4} from 'uuid';
+
+const socket = io();
 
 export default function Chatscreen() {
     const [messages, setMessages] = useState<{ id: string; text: string }[]>([]);
     const [input, setInput] = useState('');
 
+    
+    useEffect(() => {
+        socket.on('chat message', (message: { id: string; text: string }) => {
+            setMessages(prev => [...prev, message]);
+        });
+        
+        return () => {
+            socket.off('chat message');
+        };
+    }, []);
+
     const sendMessage = () => {
     if (input.trim() !== '') {
+            const message = {date: Date.now().toString(), text: input};
+            socket.emit('send message', message);
             setMessages([...messages, { id: Date.now().toString(), text: input }]);
             setInput('');
         }
