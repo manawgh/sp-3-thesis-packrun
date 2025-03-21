f13712e99f10ae98b58ee4bf2515aad0e11e2850
David-Torre-Ubuntu <sobacoman@hotmail.com>
2025-03-21 17:52:28 +0100

    deleted the client inside server. Somebody put it by mistake

--- server/client/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-// https://docs.expo.dev/guides/using-eslint/
-module.exports = {
-  extends: ['expo', 'prettier'],
-  pluggins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error'
-  },
-  ignorePatterns: ['/dist/*'],
-};
