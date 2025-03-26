554393356249690bc5299a7f9c6c9af58bc05b77
archiemt <archiemt7@gmail.com>
2025-03-26 13:55:44 +0000

    merge markers

--- server/src/models/model.ts
+++ server/src/models/model.ts
@@ -3,15 +3,9 @@ import { Sequelize } from "sequelize";
 const sequelize: Sequelize = new Sequelize({
   dialect: "postgres",
   host: "localhost",
-<<<<<<< HEAD
   username: "postgres",
   password: "OrangeFire393.",
   database: "packrundb",
-=======
-  username: "db_user",
-  password: "1243",
-  database: "packRunDB",
->>>>>>> d76fc1e4d6990c40b13a982ca834d090621b290d
 });
 
 export default sequelize; 
