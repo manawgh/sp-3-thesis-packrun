9b4a98380d1c182b08fe43b7b9810123d59cd19d
David-Torre-Ubuntu <sobacoman@hotmail.com>
2025-03-26 20:09:51 +0100

    fixed bug that was preventing deletion of tables when more than 1 user logouts at the same time

--- server/src/controllers/loginController.ts
+++ server/src/controllers/loginController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 import RunnerModel, { Runner } from "../models/runnerModel";
 import { removeRunnerFromChatRoom } from "../helpers/chatFunctions";
+import { Op } from "sequelize";
+import ChatRoomModel from "../models/chatRoomModel";
 
 //minutes of inactivity to autologout users
 const LOGIN_EXPIRES_MINUTES = 5;
@@ -25,46 +27,46 @@ export async function logUser(req: Request, res: Response, next: Function) {
 }
 
 function isMissingFields(req: Request): boolean {
-  return !req.body || Object.keys(req.body).length === 0
-    || !Object.keys(req.body).includes('longitude') || !Object.keys(req.body).includes('latitude')
-    || !Object.keys(req.body).includes('userId')
+  /*  return !req.body || Object.keys(req.body).length === 0
+     || !Object.keys(req.body).includes('longitude') || !Object.keys(req.body).includes('latitude')
+     || !Object.keys(req.body).includes('userId') */
 
-  /*//! next incoming format 
   return !req.body || Object.keys(req.body).length === 0
     || !Object.keys(req.body).includes('userId') || !Object.keys(req.body).includes('coords')
     || !Object.keys(req.body.coords).includes('longitude')
-    || !Object.keys(req.body.coords).includes('latitude'); 
-    */
+    || !Object.keys(req.body.coords).includes('latitude');
+
 }
 
 function incorrectCoordinates(req: Request) {
   return req.body.latitude < -90 || req.body.latitude > 90 || req.body.longitude < -180 || req.body.longitude > 180;
 }
 
-function checkExpiringSessions() {
+async function checkExpiringSessions() {
 
-  const now = Date.now();
+  const expiringRunners = await RunnerModel
+    .findAll({ where: { updatedAt: { [Op.lt]: new Date(Date.now() - LOGIN_EXPIRES_MINUTES * 60 * 1000) } } });
 
-  RunnerModel.findAll()
-    .then(res => res.forEach(runner => {
-      if (!runner.dataValues.updatedAt) logoutUser(runner);
-      else now - runner.dataValues.updatedAt.getTime() >= LOGIN_EXPIRES_MINUTES * 1000 * 60 && logoutUser(runner);
-    }))
-    .catch(err => console.log('ERROR', err));
+  if (expiringRunners && expiringRunners.length !== 0) {
+    Promise.all(expiringRunners.map(async (runner) => removeRunnerFromChatRoom(runner.userId, runner.assignedChatRoom)))
+      .then(() => checkForEmptyChatRooms())
+      .catch(err => console.log(err));
+  } else console.log('nada aun');
+  await RunnerModel.destroy({ where: { updatedAt: { [Op.lt]: new Date(Date.now() - LOGIN_EXPIRES_MINUTES * 60 * 1000) } } });
 
 }
-async function logoutUser(runner: Runner) {
-  const runnerEntry = await RunnerModel.findOne({ where: { userId: runner.userId } });
-  if (runnerEntry) {
-    await removeRunnerFromChatRoom(runner.userId, runnerEntry.assignedChatRoom);
-    runnerEntry.destroy();
-  }
+
+async function checkForEmptyChatRooms() {
+  const chatRooms = await ChatRoomModel.findAll();
+  chatRooms.forEach(room => room.usersId.length === 0 && room.destroy());
 }
+
 export async function checkIfLogged(req: Request, res: Response, next: Function) {
   const userId = req.params.userId;
   const isLoggedIn = await RunnerModel.findOne({ where: { userId } });
 
-  isLoggedIn? next(): res.status(400).send('User not logged in');
+  isLoggedIn ? next() : res.status(400).send('User not logged in');
 
 }
+
 setInterval(checkExpiringSessions, 1000 * 60 * LOGIN_EXPIRES_MINUTES / 2);
